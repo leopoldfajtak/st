9a9a9f07156028b8ee5cfb08c660add552f4198c
Leopold Fajtak <leopoldfajtak@leopoldws.leopoldfajtak>
2021-05-05 12:03:08 +0200

    patch font2

--- config.h
+++ config.h
@@ -5,13 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-/* Spare fonts */
-static char *font2[] = {
-/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
-};
-
+static char *font = "Inconsolata for Powerline:pixelsize=15:antialias=true:autohint=true";
+static char *font2[] = {"Inconsolata for Powerline:pixelsize=15:antialias=true:autohint=true"};
+//static char *font = "mononoki Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
+//static char *font2[] = {"mononoki Nerd Font Mono:pixelsize=15:antialias=true:autohint=true"};
 static int borderpx = 2;
 
 /*
