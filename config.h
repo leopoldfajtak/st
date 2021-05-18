4531905748a4f8df6c66e19053b67d990f14071d
Leopold Fajtak <leopoldfajtak@leopoldws.leopoldfajtak>
2021-05-18 10:55:40 +0200

    Changed fontsize

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Inconsolata for Powerline:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = {"Sybola:pixelsize=15:antialias=true:autohint=true"};
+static char *font = "Inconsolata for Powerline:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = {"Sybola:pixelsize=18:antialias=true:autohint=true"};
 
 static int borderpx = 0;
 
